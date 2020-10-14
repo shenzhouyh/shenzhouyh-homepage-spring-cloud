956701459b915773ac62f7ea625a66970a610e51
shenzhouyh <286817442@qq.com>
2020-10-14 22:04:02 +0800

    Initial commit

---
+++ homepage-eureka/src/main/java/com/shenzhouyh/homepage/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.shenzhouyh.homepage;
+
+/**
+ * @description :
+ * @since : 10.7.0
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
